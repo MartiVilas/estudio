7ebe738b1c416dc348e7871a57cceb251d0613e6
Martí Vilàs <marti.vilas14@gmail.com>
2025-02-21 13:43:01 +0100

    changes

---
+++ Objetos/ObjetosArray.js
@@ -0,0 +1,24 @@
+const diasSemana = ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"];
+
+
+
+
+function encontrarPrimeraM(){
+    
+    for (let i = 0; i < diasSemana.length; i++) {
+         diasSemana[i].find()
+        
+    }
+
+
+}
+
+
+
+function init(){
+
+
+
+
+}
+
