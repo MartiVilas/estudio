187318056c290c647f1c918ecb8572c70bae49fb
MartiVilas <marti.vilas14@gmail.com>
2025-02-28 13:53:46 +0100

    create classes

---
+++ Objetos/JS/targetaDeCredito.js
@@ -0,0 +1,13 @@
+class targetaDeCredito{
+    numTargeta;
+    #cvv;
+
+
+    constructor(numTargeta,fechaCaducidad){
+        this.numTargeta = numTargeta;
+        this.#cvv
+        this.fechaCaducidad;
+    }
+
+
+}
